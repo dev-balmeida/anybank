d59e6d8942503edb931d64335e90c1a8b4d79c61
dev-balmeida <beatrizdealmeida00@outlook.com>
2026-03-09 08:16:54 -0300

    Criando o projeto no Supabase

---
+++ src/infra/supabase/config.ts
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supaBaseUrl: string = import.meta.env.VITE_SUPABASE_URL;
+const supaBaseKey: string = import.meta.env.VITE_SUPABASE_KEY;
+
+
+export const supabase = createClient(supaBaseUrl, supaBaseKey)
